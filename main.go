f6fb98135c5249be730051ed0a935815d63af2ad
Juna <itujun7@gmail.com>
2025-07-31 23:53:18 +0700

    Fiber Config

--- main.go
+++ main.go
@@ -1,9 +1,17 @@
 package main
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"time"
+
+	"github.com/gofiber/fiber/v2"
+)
 
 func main() {
-	app := fiber.New()
+	app := fiber.New(fiber.Config{
+		IdleTimeout: time.Second * 5, // Set idle timeout to 5 seconds
+		ReadTimeout: time.Second * 5, // Set read timeout to 5 seconds
+		WriteTimeout: time.Second * 5, // Set write timeout to 5 seconds
+	})
 
 	err := app.Listen("localhost:3000")
 	if err != nil {
