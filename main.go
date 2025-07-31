7b80926ee6141e1d13490cbe35269bc8accfb0cc
Juna <itujun7@gmail.com>
2025-08-01 02:02:06 +0700

    Fiber Pre Fork

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"time"
 
 	"github.com/gofiber/fiber/v2"
@@ -11,11 +12,20 @@ func main() {
 		IdleTimeout: time.Second * 5, // Set idle timeout to 5 seconds
 		ReadTimeout: time.Second * 5, // Set read timeout to 5 seconds
 		WriteTimeout: time.Second * 5, // Set write timeout to 5 seconds
+		Prefork: true, // Enable pre-forking
 	})
 
 	app.Get("/", func(ctx *fiber.Ctx) error {
 		return ctx.SendString("Hello, World!")
 	})
+
+	if fiber.IsChild(){
+		fmt.Println("I'am child process");
+	}else{
+		fmt.Println("I'am parent process");
+	}
+	// jalankan server, lalu cek task manager
+	// maka akan menemunkan total 5 process dari 1 proses parent dan 4 proses child
 
 	err := app.Listen("localhost:3000")
 	if err != nil {
