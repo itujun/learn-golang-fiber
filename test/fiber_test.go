a3735af10db053f704b5fc52ac51bb2ac8e0f011
Juna <itujun7@gmail.com>
2025-08-01 00:15:09 +0700

    Fiber Ctx

--- test/fiber_test.go
+++ test/fiber_test.go
@@ -9,8 +9,9 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+var	app = fiber.New()
+
 func TestRoutingHelloWorld(t *testing.T) {
-	app := fiber.New()
 	app.Get("/", func(c *fiber.Ctx) error {
 		return c.SendString("Hello, World!")
 	})
@@ -24,4 +25,31 @@ func TestRoutingHelloWorld(t *testing.T) {
 	bytes, err :=io.ReadAll(response.Body)
 	assert.Nil(t, err)
 	assert.Equal(t, "Hello, World!", string(bytes))
+}
+
+func TestCtx(t *testing.T) {
+	app.Get("/hello", func(c *fiber.Ctx) error {
+		name := c.Query("name", "guest") // Default to "World" if no name is provided
+		return c.SendString("Hello, " + name + "!")
+	})
+
+	request := httptest.NewRequest("GET", "/hello?name=Lev", nil)
+	response, err := app.Test(request)
+
+	assert.Nil(t, err)
+	assert.Equal(t, 200, response.StatusCode)
+
+	bytes, err :=io.ReadAll(response.Body)
+	assert.Nil(t, err)
+	assert.Equal(t, "Hello, Lev!", string(bytes))
+
+	request = httptest.NewRequest("GET", "/hello", nil)
+	response, err = app.Test(request)
+
+	assert.Nil(t, err)
+	assert.Equal(t, 200, response.StatusCode)
+
+	bytes, err =io.ReadAll(response.Body)
+	assert.Nil(t, err)
+	assert.Equal(t, "Hello, guest!", string(bytes))
 }
